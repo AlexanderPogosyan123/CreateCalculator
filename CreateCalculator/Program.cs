43b9e96af0c7b7dc264a55b1e5f9234814230549
Alexander Pogosyan <aleksandrpogosyan76@gmail.com>
2023-09-02 14:32:02 +0400

    Add project files.

---
+++ CreateCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CreateCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
