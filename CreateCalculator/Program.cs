cb7dbc976ae91d637674193fdcb807e5cc8595e1
Alexander Pogosyan <aleksandrpogosyan76@gmail.com>
2023-09-02 14:34:46 +0400

    добавил ввод пользователя

--- CreateCalculator/Program.cs
+++ CreateCalculator/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("введите первое число: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите операцию: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("введите второе число: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+
         }
     }
 }
